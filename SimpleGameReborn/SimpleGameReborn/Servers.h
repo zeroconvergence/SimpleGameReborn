60493ea266f2b681d651057f4cc3d4b7cc9db050
0x6k7 <78702905+0x6k7@users.noreply.github.com>
2023-07-30 15:51:01 +0300

    Delete SimpleGameReborn directory

--- SimpleGameReborn/SimpleGameReborn/Servers.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-using std::string;
-
-string TestServerIP = "143.50.23.26";
-string ISPWebServerIP = "11.11.111.1";
--- SimpleGameReborn/Utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-bool is_int(const std::string& str) {
-	std::string::const_iterator it = str.begin();
-	while (it != str.end() && std::isdigit(*it)) ++it;
-	return !str.empty() && it == str.end();
-}
