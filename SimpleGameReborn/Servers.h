004737840efe5f71bb49a39083eeb7ffe8a48796
0x6k7 <78702905+0x6k7@users.noreply.github.com>
2023-07-30 15:51:49 +0300

    Update SimpleGameReborn
    TODO: Changelog

---
+++ SimpleGameReborn/Servers.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+using std::string;
+
+string TestServerIP = "143.50.23.26";
+string ISPWebServerIP = "11.11.111.1";
---
+++ SimpleGameReborn/Timer.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <thread>
+
+void NormalTracker() {
+	 //start = 20;
+	/*while (start != 0) {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		start--;
+		if(start < 1)
+			cout << start;
+	}*/
+	// Tracked! Attacker's IP has been sent to the FBI
+}
